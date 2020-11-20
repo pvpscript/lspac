a4ca299a5685a422515ecffa89fe4fb761675ee0
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-11-20 20:30:00 -0300

    No need to cast

--- op.c
+++ op.c
@@ -6,84 +6,60 @@
 
 struct container *lspac_pkg_get_name(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_name(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_name(data), prefix);
 }
 
 struct container *lspac_pkg_get_base(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_base(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_base(data), prefix);
 }
 
 struct container *lspac_db_get_name(void *data, char *prefix)
 {
-	alpm_db_t *db = (alpm_db_t *)data;
-
-	return str_to_container((char *)alpm_db_get_name(db), prefix);
+	return str_to_container((char *)alpm_db_get_name(data), prefix);
 }
 
 struct container *lspac_pkg_get_filename(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_filename(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_filename(data), prefix);
 }
 
 struct container *lspac_pkg_get_version(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_version(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_version(data), prefix);
 }
 
 struct container *lspac_pkg_get_desc(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_desc(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_desc(data), prefix);
 }
 
 struct container *lspac_pkg_get_arch(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_arch(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_arch(data), prefix);
 }
 
 struct container *lspac_pkg_get_url(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_url(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_url(data), prefix);
 }
 
 struct container *lspac_pkg_get_packager(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_packager(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_packager(data), prefix);
 }
 
 struct container *lspac_pkg_get_md5sum(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_md5sum(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_md5sum(data), prefix);
 }
 
 struct container *lspac_pkg_get_sha256sum(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_sha256sum(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_sha256sum(data), prefix);
 }
 
 struct container *lspac_pkg_get_base64_sig(void *data, char *prefix)
 {
-	alpm_pkg_t *pkg = (alpm_pkg_t *)data;
-
-	return str_to_container((char *)alpm_pkg_get_base64_sig(pkg), prefix);
+	return str_to_container((char *)alpm_pkg_get_base64_sig(data), prefix);
 }
