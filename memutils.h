1115b6a5f89723040e59d9f61320a30ccc71b8b3
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-11-20 21:12:29 -0300

    Added commentary

--- memutils.h
+++ memutils.h
@@ -1,7 +1,16 @@
 #ifndef MEMUTILS_H
 #define MEMUTILS_H
 
+/* 
+ * A malloc wrapper that tests for allocation errors.
+ * In case of an error, it finishes the program.
+ */
 void *c_malloc(size_t size);
+
+/* 
+ * A realloc wrapper that tests for allocation errors.
+ * In case of an error, it finishes the program.
+ */
 void *c_realloc(void *ptr, size_t size);
 
 #endif /* MEMUTILS_H */
