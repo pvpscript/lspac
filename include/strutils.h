14a1b772b8963c6e986cd7f1458df63c76d272f4
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-11-24 19:56:55 -0300

    refactor: modified meta foreach loops

--- include/strutils.h
+++ include/strutils.h
@@ -1,12 +1,11 @@
 #ifndef STRUTILS_H
 #define STRUTILS_H
 
-#define FOREACH_STR_DESTRUCTIVE(str, index, arr) \
-        for ((index) = 0; (arr) && ((str) = ((arr)[(index)])); \
-             free((str)), (index)++)
+#define FOREACH_STR(str, arr_ptr) \
+	for (; (arr_ptr) && ((str) = *(arr_ptr)); (arr_ptr)++)
 
-#define FOREACH_STR(str, index, arr) \
-        for ((index) = 0; (arr) && ((str) = ((arr)[(index)])); (index)++)
+#define FOREACH_STR_DESTRUCTIVE(str, arr_ptr) \
+	for (; (arr_ptr) && ((str) = *(arr_ptr)); free((str)), (arr_ptr)++)
 
 
 int add_out_to_idarray(char *list, int *arr, size_t arrsz, size_t *arr_pos,
