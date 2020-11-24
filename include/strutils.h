8e45fcd388de86548c4dc8c490a372de54f4fc0b
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-11-23 23:31:05 -0300

    Moved str meta loops to here

--- include/strutils.h
+++ include/strutils.h
@@ -1,6 +1,14 @@
 #ifndef STRUTILS_H
 #define STRUTILS_H
 
+#define FOREACH_STR_DESTRUCTIVE(str, index, arr) \
+        for ((index) = 0; (arr) && ((str) = ((arr)[(index)])); \
+             free((str)), (index)++)
+
+#define FOREACH_STR(str, index, arr) \
+        for ((index) = 0; (arr) && ((str) = ((arr)[(index)])); (index)++)
+
+
 int add_out_to_idarray(char *list, int *arr, size_t arrsz, size_t *arr_pos,
                        int name_to_id(const char *, size_t));
 
